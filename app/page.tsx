e073caeb3d25df343abb1622ded853795428d10d
mauassenco <mau.assenco@gmail.com>
2024-05-04 17:19:26 -0300

    chore: add husky and lint staged setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
-export default function Home() {
+const Home = () => {
   return (
-    <div className="flex px-3 bg-red-500">
+    <div className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
       <h1>Hello Tailwind</h1>
-    </div >
+    </div>
   );
-}
+};
+
+export default Home;
