33d0e2b748a52479988d7e77dc9c7509484d3aca
mauassenco <mau.assenco@gmail.com>
2024-05-06 16:30:28 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "./_components/header";
+
 const Home = () => {
   return (
-    <div className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-      <h1>Hello Tailwind</h1>
+    <div>
+      <Header />
     </div>
   );
 };
