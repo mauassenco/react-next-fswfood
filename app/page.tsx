fb01b81a9e2b2484567542e7eee912bcc7e745be
mauassenco <mau.assenco@gmail.com>
2024-05-06 21:39:37 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Header />
 
       <div className=" px-5 pt-6">
@@ -14,7 +15,19 @@ const Home = () => {
       <div className="px-5 pt-6">
         <CategoryList />
       </div>
-    </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizza"
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
+    </>
   );
 };
 
