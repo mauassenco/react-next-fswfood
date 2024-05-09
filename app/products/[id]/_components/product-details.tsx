e661abd803b426ebaee056775009eb9f3bf66c5e
mauassenco <mau.assenco@gmail.com>
2024-05-09 14:49:04 -0300

    style: add border radius effects to product details component

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -47,7 +47,7 @@ const ProductDetails = ({
     });
 
   return (
-    <div className="py-5">
+    <div className="relative z-50 mt-[-1.5rem] rounded-tl-3xl rounded-tr-3xl bg-white py-5">
       <div className="flex items-center gap-[0.375rem] px-5">
         <div className="relative h-6 w-6">
           <Image
@@ -132,10 +132,12 @@ const ProductDetails = ({
         <p className="text-sm text-muted-foreground">{product.description}</p>
       </div>
 
-      <div className="mt-6 space-y-3">
-        <h3 className="px-5 font-semibold">Sucos</h3>
-        <ProductList products={complemetaryProducts} />
-      </div>
+      {complemetaryProducts.length > 0 && (
+        <div className="mt-6 space-y-3">
+          <h3 className="px-5 font-semibold">Sucos</h3>
+          <ProductList products={complemetaryProducts} />
+        </div>
+      )}
     </div>
   );
 };
